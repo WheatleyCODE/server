3f557fffab9fd1abb5577c87b60a1a4101c79678
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2022-03-06 20:15:58 +0300

    Create default project, add jwt, swagger, env, validator and more libs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    app.use(cookieParser());
+    app.enableCors({
+      credentials: true,
+      origin: process.env.API_CLIENT_IP,
+    });
+
+    const config = new DocumentBuilder()
+      .setTitle('JETCOLOR')
+      .setDescription('JETCOLOR project')
+      .setVersion('1.0.0')
+      .addTag('JET PROJECT')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+
+    await app.listen(PORT, () =>
+      console.log('Server has been started on port:' + PORT),
+    );
+  } catch (e) {
+    console.log(e);
+  }
 }
-bootstrap();
+
+start();
